7692c76b53f70ea6c8de7c000097ac11d7f12331
Luciano Oliveira and Safiy Cham <safiy@>
2017-08-24 09:18:01 -0700

    switch player logic and win lose message

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,7 @@ function Player(name){
 }
 
 function Dice(){
-  this.value = 1;
+  this.value = 0;
 }
 
 //Add constructor Methods
@@ -20,7 +20,7 @@ Player.prototype.addTurnToScore = function(){
 }
 
 $(function(){
-  var scoreToWin = 50;
+  var scoreToWin = 10;
   var die = new Dice();
   var player1 = new Player("player 1");
   var player2  = new Player("player 2");
@@ -28,6 +28,15 @@ $(function(){
 
   updateGame();
 
+  function getActivePlayer(){
+    if(isP1Turn) {
+      return player1;
+    } else {
+      return player2;
+    }
+  }
+
+//Interface Logic
   $(".roll").click(function(){
     die.roll();
     if(die.value === 1){
@@ -44,8 +53,6 @@ $(function(){
     getActivePlayer().turnTotal = 0;
 
     if(getActivePlayer().score >= scoreToWin){
-      $("#p1-score").css('background-color', red);
-      $("#p1-score").css('background-color', red);
       $('#dice').hide();
       $('#p1-total').text(player1.score);
       $('#p1-current').text(player1.turnTotal);
@@ -53,11 +60,13 @@ $(function(){
       $('#p2-current').text(player2.turnTotal);
 
       if(isP1Turn){
-        $("#p1-results").text("You Win!");
+        var check = $("#p1-results").text("You Win!");
+        console.log(check);
         $("#p2-results").text("You Lose!");
       } else {
         $("#p1-results").text("You Lose!");
-        $("#p2-results").text("You Win!");
+        var check2 = $("#p2-results").text("You Win!");
+        console.log(check2);
       }
     } else {
       switchTurn();
@@ -67,11 +76,11 @@ $(function(){
 
   function switchTurn(){
     if(isP1Turn = !isP1Turn) {
-      $('.player2').toggleClass('activeClass');
-      $('.player1').toggleClass('inactiveClass');
+      $('.player1').addClass('activeClass');
+      $('.player2').removeClass('activeClass');
     } else {
-      $('.player2').toggleClass('inactiveClass');
-      $('.player1').toggleClass('activeClass');
+      $('.player1').removeClass('activeClass');
+      $('.player2').addClass('activeClass');
     }
     //some sort of toggle
     // $('.player1 .player2').toggleClass('activeClass');
@@ -84,12 +93,6 @@ $(function(){
     $('#p2-current').text(player2.turnTotal);
   }
 
-  function getActivePlayer(){
-    if(isP1Turn) {
-      return player1;
-    } else {
-      return player2;
-    }
-  }
+
 
 });
